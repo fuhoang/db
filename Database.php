4f062dc5e436754cb8870eb44573390c0fdb7328
Fu Hoang <fu.hoang@mavad.co.uk>
2017-06-12 15:49:27 +0100

    database class

--- Database.php
+++ Database.php
@@ -9,7 +9,50 @@ class Db {
 		$config = parse_ini_file('./config.ini');
 		self::$connection = new mysqli('localhost', $config['username'], $config['password'], $config['dbname']);
 
-		print_r($config);
+		if(self::$connection == false) {
+			// Handler error - notify administrator, log to a file, show an error screen, etc. 
+			echo "Database error";
+			return false;
+		}
+		return self::$connection;
 	}
+
+	public function query($query) {
+
+		//Connect to the database
+		$connection= $this->connect();
+
+		//Query the database
+		$result = $connection->query($query);
+	
+		return $result;
+	}
+
+	public function select($query) {
+		
+		$row = array();
+		$result = $this->query($query);
+
+		if($result === false) {
+			return false;
+		}
+
+		while( $row = $result->fetch_assoc()){
+			$rows[] = $row
+		}
+
+		return $rows;
+	}
+
+	public function error(){
+		$connection = $this->connect();
+		return $connection->error;
+	}
+
+	public function quote($value) {
+		$connection = $this->connect();
+		return " ' " . $connection->real_escape_string($value) ." ' ";
+	}
+
 }
 ?>
